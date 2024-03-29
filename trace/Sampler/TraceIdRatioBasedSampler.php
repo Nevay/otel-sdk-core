365321561fb7493c1014481ffbc926bd8bcd261a
Tobias Bachert <git@b-privat.de>
2024-03-29 03:37:24 +0100

    Update ratio based sampler threshold algorithm
    See OTEP 235, Threshold = (1 - Probability) * 2**56.

--- trace/Sampler/TraceIdRatioBasedSampler.php
+++ trace/Sampler/TraceIdRatioBasedSampler.php
@@ -8,8 +8,14 @@ use Nevay\OTelSDK\Trace\SamplingDecision;
 use Nevay\OTelSDK\Trace\SamplingResult;
 use Nevay\OTelSDK\Trace\Span\Kind;
 use OpenTelemetry\Context\ContextInterface;
+use function assert;
+use function max;
+use function min;
+use function pack;
 use function sprintf;
+use function substr;
 use function substr_compare;
+use function unpack;
 
 final class TraceIdRatioBasedSampler implements Sampler {
 
@@ -25,7 +31,7 @@ final class TraceIdRatioBasedSampler implements Sampler {
         }
 
         $this->ratio = $ratio;
-        $this->threshold = substr(pack('J', (int) ($ratio * (1 << 56))), 1);
+        $this->threshold = substr(pack('J', self::computeTValue($ratio, minPrecision: 14, bitPrecision: 12)), 1);
     }
 
     public function shouldSample(
@@ -36,12 +42,60 @@ final class TraceIdRatioBasedSampler implements Sampler {
         Attributes $attributes,
         array $links,
     ): SamplingResult {
-        return $this->ratio === 1. || substr_compare($traceId, $this->threshold, 9, 7) < 0
+        return $this->ratio >= 2 ** -56 && substr_compare($traceId, $this->threshold, 9) >= 0
             ? SamplingDecision::RecordAndSample
             : SamplingDecision::Drop;
     }
 
     public function __toString(): string {
         return sprintf('TraceIdRatioBased{%F}', $this->ratio);
+    }
+
+    /**
+     * Computes the 56-bit rejection threshold (T-value) for a given probability.
+     *
+     * The T-value is computed as `2**56*(1-$probability)` with a precision of
+     * `2**-(4*Max(⌈(-log2($probability)+$bitPrecision)/4⌉,$minPrecision))`.
+     *
+     * Values below `2**-56` will return `0`.
+     *
+     * ```
+     * 1/3 w/ $minPrecision=3
+     * => 1 - 1/3
+     * => 2/3
+     * => 2730.666../4096
+     * => 2731/4096
+     * => 0xaab
+     * ```
+     *
+     * Converting the result into `th` hexadecimal value:
+     * ```
+     * $th = rtrim(bin2hex(substr(pack('J', $t), 1)), '0') ?: '0';
+     * ```
+     *
+     * @param float $probability sampling probability, must be between 0 and 1
+     * @param int $minPrecision minimum precision in hexadecimal digits
+     * @param int $bitPrecision precision increase in bits
+     * @return int 56bit T-value
+     */
+    private static function computeTValue(float $probability, int $minPrecision = 0, int $bitPrecision = 0): int {
+        assert($probability >= 0 && $probability <= 1);
+        assert($minPrecision >= 0);
+        assert($bitPrecision >= 0);
+
+        $b = unpack('J', pack('E', $probability))[1];
+        $e = $b >> 52 & (1 << 11) - 1;
+        $f = $b & (1 << 52) - 1 | !!$e << 52;
+
+        // 56+1bit for rounding
+        $s = $e - 1023 - 52 + 57;
+        $t = (1 << 57) - ($s < 0 ? $f >> -$s : $f << $s);
+
+        // minimum precision in hexadecimal digits
+        $p = -($e - 1023) + 3 + $bitPrecision >> 2;
+        $p = min(14, max($minPrecision, $p));
+        $m = 1 << (14 - $p << 2);
+
+        return $t + $m >> 1 & -$m;
     }
 }
