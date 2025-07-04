589fda1719dd993de204faba48016e15ad7edcfe
Tobias Bachert <git@b-privat.de>
2025-07-04 23:57:08 +0200

    Drop PHP8.1

--- metrics/Exemplar/SimpleFixedSizeExemplarReservoir.php
+++ metrics/Exemplar/SimpleFixedSizeExemplarReservoir.php
@@ -6,38 +6,27 @@ use Nevay\OTelSDK\Metrics\ExemplarReservoir;
 use Nevay\OTelSDK\Metrics\Internal\Exemplar\BucketStorage;
 use OpenTelemetry\Context\ContextInterface;
 use Random\Engine\PcgOneseq128XslRr64;
-use Random\RandomException;
 use Random\Randomizer;
-use function mt_rand;
-use const PHP_VERSION_ID;
 
 final class SimpleFixedSizeExemplarReservoir implements ExemplarReservoir {
 
-    private readonly ?Randomizer $randomizer;
+    private readonly Randomizer $randomizer;
     private readonly BucketStorage $storage;
     private readonly int $size;
 
     private int $measurements = 0;
 
-    public function __construct(int $size, ?Randomizer $randomizer = null) {
-        if (PHP_VERSION_ID >= 80200) {
-            $randomizer ??= new Randomizer(new PcgOneseq128XslRr64());
-        }
-
+    public function __construct(int $size, Randomizer $randomizer = new Randomizer(new PcgOneseq128XslRr64())) {
         $this->randomizer = $randomizer;
         $this->storage = new BucketStorage($size);
         $this->size = $size;
     }
 
     public function offer(float|int $value, Attributes $attributes, ContextInterface $context, int $timestamp): void {
-        if ($this->measurements < $this->size) {
-            $bucket = $this->measurements;
-        } else {
-            try {
-                $bucket = $this->randomizer?->getInt(0, $this->measurements);
-            } catch (RandomException) {}
-            $bucket ??= mt_rand(0, $this->measurements);
-        }
+        $bucket = $this->measurements < $this->size
+            ? $this->measurements
+            : $this->randomizer->getInt(0, $this->measurements);
+
         $this->measurements++;
         if ($bucket < $this->size) {
             $this->storage->store($bucket, $value, $attributes, $context, $timestamp);
--- trace/IdGenerator/RandomIdGenerator.php
+++ trace/IdGenerator/RandomIdGenerator.php
@@ -3,75 +3,36 @@ namespace Nevay\OTelSDK\Trace\IdGenerator;
 
 use Nevay\OTelSDK\Trace\IdGenerator;
 use Random\Engine\PcgOneseq128XslRr64;
-use Random\RandomException;
 use Random\Randomizer;
-use function assert;
-use function mt_getrandmax;
-use function mt_rand;
-use function pack;
-use function strspn;
-use const PHP_VERSION_ID;
 
 final class RandomIdGenerator implements IdGenerator {
 
-    private readonly ?Randomizer $randomizer;
+    private readonly Randomizer $randomizer;
 
     /**
-     * @param Randomizer|null $randomizer randomizer to use
+     * @param Randomizer $randomizer randomizer to use
      */
-    public function __construct(?Randomizer $randomizer = null) {
-        if (PHP_VERSION_ID >= 80200) {
-            $randomizer ??= new Randomizer(new PcgOneseq128XslRr64());
-        }
-
+    public function __construct(Randomizer $randomizer = new Randomizer(new PcgOneseq128XslRr64())) {
         $this->randomizer = $randomizer;
     }
 
     public function generateSpanIdBinary(): string {
-        if ($this->randomizer) {
-            try {
-                return self::generateBytes($this->randomizer, 8);
-            } catch (RandomException) {}
-        }
-
-        assert(($n = mt_getrandmax()) >= (1 << 31) - 1 && !($n & $n + 1));
         do {
-            $c = mt_rand();
-            $r = mt_rand() ^ mt_rand() << 31 ^ $c << 62;
-        } while (!$r);
+            $bytes = $this->randomizer->getBytes(8);
+        } while ($bytes === "\0\0\0\0\0\0\0\0");
 
-        return pack('Q', $r);
+        return $bytes;
     }
 
     public function generateTraceIdBinary(): string {
-        if ($this->randomizer) {
-            try {
-                return $this->generateBytes($this->randomizer, 16);
-            } catch (RandomException) {}
-        }
-
-        assert(($n = mt_getrandmax()) >= (1 << 31) - 1 && !($n & $n + 1));
         do {
-            $c = mt_rand();
-            $hi = mt_rand() ^ mt_rand() << 31 ^ $c << 62;
-            $lo = mt_rand() ^ mt_rand() << 31 ^ $c << 60;
-        } while (!$hi && !$lo);
+            $bytes = $this->randomizer->getBytes(16);
+        } while ($bytes === "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
 
-        return pack('Q2', $hi, $lo);
+        return $bytes;
     }
 
     public function traceFlags(): int {
         return 0x2;
-    }
-
-    /**
-     * @param positive-int $length
-     */
-    private static function generateBytes(Randomizer $randomizer, int $length): string {
-        do {
-            $bytes = $randomizer->getBytes($length);
-        } while (strspn($bytes, "\0") === $length);
-
-        return $bytes;
     }
 }
