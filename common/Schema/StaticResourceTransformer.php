c7a8a4b03420183f2c7ed54a95ce5f50eb7ca8a3
Tobias Bachert <git@b-privat.de>
2026-05-07 21:47:10 +0200

    Update resource transformer to schema `1.41.0`

--- common/Schema/StaticResourceTransformer.php
+++ common/Schema/StaticResourceTransformer.php
@@ -35,7 +35,7 @@ final class StaticResourceTransformer implements ResourceTransformer {
      * @see https://opentelemetry.io/docs/specs/semconv/resource/
      */
     public static function opentelemetrySchema(): ResourceTransformer {
-        return self::https_opentelemetry_io_schemas_1_40_0();
+        return self::https_opentelemetry_io_schemas_1_41_0();
     }
 
     /**
@@ -180,9 +180,9 @@ final class StaticResourceTransformer implements ResourceTransformer {
         return substr($schemaUrl, $separator + 1);
     }
 
-    private static function https_opentelemetry_io_schemas_1_40_0(): ResourceTransformer {
+    private static function https_opentelemetry_io_schemas_1_41_0(): ResourceTransformer {
         return new StaticResourceTransformer(
-            schemaUrl: 'https://opentelemetry.io/schemas/1.40.0',
+            schemaUrl: 'https://opentelemetry.io/schemas/1.41.0',
             versions: [
                 '1.4.0' => 0,
                 '1.5.0' => 0,
@@ -222,6 +222,7 @@ final class StaticResourceTransformer implements ResourceTransformer {
                 '1.38.0' => 21,
                 '1.39.0' => 22,
                 '1.40.0' => 23,
+                '1.41.0' => 23,
             ],
             attributeMaps: [
                 [
